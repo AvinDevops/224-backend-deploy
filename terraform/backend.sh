4a9fb3f3b946ed2a75cee5eeffe3d95f1f5fc694
AvinDevops <avinashbandela73@gmail.com>
2025-12-19 16:19:02 +0530

    backend-deploy

--- terraform/backend.sh
+++ terraform/backend.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 component=$1
 environment=$2
+app_version=$3
 dnf install ansible -y 
 pip3.9 install boto3 botocore
-ansible-pull -i localhost, -U https://github.com/AvinDevops/220-expense-ansible-roles-tf.git main.yaml -e component=$component -e env=$environment
+ansible-pull -i localhost, -U https://github.com/AvinDevops/220-expense-ansible-roles-tf.git main.yaml -e component=$component -e env=$environment -e appVersion=$app_version
